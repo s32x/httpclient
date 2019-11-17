e8ecff7e248f1635723f4e37a9db1392eb1f5a82
Steven Wolfe <steven@swolfe.me>
2019-11-17 02:23:03 -0700

    Test without testify

--- client_test.go
+++ client_test.go
@@ -2,69 +2,223 @@ package httpclient
 
 import (
 	"net/http"
+	"reflect"
 	"sync"
 	"testing"
 	"time"
-
-	"github.com/stretchr/testify/assert"
 )
 
-func TestClient(t *testing.T) {
-	c := New()
-	assert.Equal(t, &Client{
-		client:  &http.Client{},
-		baseURL: "",
-		header:  sync.Map{},
-	}, c)
-
-	c = c.WithClient(&http.Client{Timeout: 20 * time.Second})
-	assert.Equal(t, &Client{
-		client:  &http.Client{Timeout: 20 * time.Second},
-		baseURL: "",
-		header:  sync.Map{},
-	}, c)
-
-	c = c.WithTimeout(time.Second * 10)
-	assert.Equal(t, &Client{
-		client:  &http.Client{Timeout: 10 * time.Second},
-		baseURL: "",
-		header:  sync.Map{},
-	}, c)
-
-	c = c.WithBaseURL("https://example.com")
-	assert.Equal(t, &Client{
-		client:  &http.Client{Timeout: 10 * time.Second},
-		baseURL: "https://example.com",
-		header:  sync.Map{},
-	}, c)
-
-	// var m sync.Map
-	// m.Store("hkey", "hval")
-
-	// c = c.WithHeader("hkey", "hval")
-	// assert.Equal(t, &Client{
-	// 	client:         &http.Client{Timeout: 10 * time.Second},
-	// 	baseURL:        "https://example.com",
-	// 	header:         m,
-	// 	expectedStatus: 200,
-	// 	retryCount:     0,
-	// }, c)
-
-	req := c.Request(http.MethodGet, "/test")
-	assert.Equal(t, &Request{
-		client:  &http.Client{Timeout: 10 * time.Second},
-		method:  "GET",
-		baseURL: "https://example.com",
-		path:    "/test",
-		header:  sync.Map{},
-		body:    nil,
-		ctx:     nil,
-	}, req)
-
-	// Verify generating the request hasn't modified the Client
-	assert.Equal(t, &Client{
-		client:  &http.Client{Timeout: 10 * time.Second},
-		baseURL: "https://example.com",
-		header:  sync.Map{},
-	}, c)
+func TestNew(t *testing.T) {
+	tests := []struct {
+		name string
+		want *Client
+	}{
+		{
+			name: "new",
+			want: &Client{
+				client: &http.Client{},
+				header: sync.Map{},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := New(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("New() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
+
+func TestClient_WithClient(t *testing.T) {
+	type fields struct {
+		client  *http.Client
+		baseURL string
+		header  sync.Map
+	}
+	type args struct {
+		client *http.Client
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   *Client
+	}{
+		{
+			name: "client with 20 second timeout",
+			fields: fields{
+				client: &http.Client{},
+				header: sync.Map{},
+			},
+			args: args{
+				client: &http.Client{Timeout: 20 * time.Second},
+			},
+			want: &Client{
+				client: &http.Client{Timeout: 20 * time.Second},
+				header: sync.Map{},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &Client{
+				client:  tt.fields.client,
+				baseURL: tt.fields.baseURL,
+				header:  tt.fields.header,
+			}
+			if got := c.WithClient(tt.args.client); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Client.WithClient() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestClient_WithTimeout(t *testing.T) {
+	type fields struct {
+		client  *http.Client
+		baseURL string
+		header  sync.Map
+	}
+	type args struct {
+		timeout time.Duration
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   *Client
+	}{
+		{
+			name: "5 second timeout",
+			fields: fields{
+				client: &http.Client{},
+				header: sync.Map{},
+			},
+			args: args{
+				timeout: 5 * time.Second,
+			},
+			want: &Client{
+				client: &http.Client{Timeout: 5 * time.Second},
+				header: sync.Map{},
+			},
+		},
+		{
+			name: "30 second timeout",
+			fields: fields{
+				client: &http.Client{},
+				header: sync.Map{},
+			},
+			args: args{
+				timeout: 30 * time.Second,
+			},
+			want: &Client{
+				client: &http.Client{Timeout: 30 * time.Second},
+				header: sync.Map{},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &Client{
+				client:  tt.fields.client,
+				baseURL: tt.fields.baseURL,
+				header:  tt.fields.header,
+			}
+			if got := c.WithTimeout(tt.args.timeout); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Client.WithTimeout() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestClient_WithBaseURL(t *testing.T) {
+	type fields struct {
+		client  *http.Client
+		baseURL string
+		header  sync.Map
+	}
+	type args struct {
+		url string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   *Client
+	}{
+		{
+			name: "example base URL",
+			fields: fields{
+				client: &http.Client{},
+				header: sync.Map{},
+			},
+			args: args{
+				url: "https://example.com",
+			},
+			want: &Client{
+				client:  &http.Client{},
+				baseURL: "https://example.com",
+				header:  sync.Map{},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &Client{
+				client:  tt.fields.client,
+				baseURL: tt.fields.baseURL,
+				header:  tt.fields.header,
+			}
+			if got := c.WithBaseURL(tt.args.url); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Client.WithBaseURL() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+// func TestClient_WithHeader(t *testing.T) {
+// 	type fields struct {
+// 		client  *http.Client
+// 		baseURL string
+// 		header  sync.Map
+// 	}
+// 	type args struct {
+// 		key   string
+// 		value string
+// 	}
+// 	tests := []struct {
+// 		name   string
+// 		fields fields
+// 		args   args
+// 		want   *Client
+// 	}{
+// 		{
+// 			name: "example base URL",
+// 			fields: fields{
+// 				client: &http.Client{},
+// 				header: sync.Map{},
+// 			},
+// 			args: args{
+// 				key:   "some_header_key",
+// 				value: "some_header_value",
+// 			},
+// 			want: &Client{
+// 				client: &http.Client{},
+// 				header: sync.Map{},
+// 			},
+// 		},
+// 	}
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			c := &Client{
+// 				client:  tt.fields.client,
+// 				baseURL: tt.fields.baseURL,
+// 				header:  tt.fields.header,
+// 			}
+// 			if got := c.WithHeader(tt.args.key, tt.args.value); !reflect.DeepEqual(got, tt.want) {
+// 				t.Errorf("Client.WithHeader() = %v, want %v", got, tt.want)
+// 			}
+// 		})
+// 	}
+// }
--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module s32x.com/httpclient
 
 go 1.13
-
-require github.com/stretchr/testify v1.4.0
